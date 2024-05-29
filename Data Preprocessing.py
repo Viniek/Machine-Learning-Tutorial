0ff761b8523d773be074ecb64b22117d8dfd5ef7
unknown <njerivictory52@gmail.com>
2024-05-29 16:36:51 +0300

    commit

--- Data Preprocessing.py
+++ Data Preprocessing.py
@@ -10,18 +10,14 @@ and encoding data so that a machine learning algorithm can understand it and use
   As the adage goes, “If garbage goes in, garbage comes out.” 
   -Your data project will only be as successful as the input 
   data you feed into your machine learning algorithms.
+
   1.It Improves Data Quality
 Data preprocessing is the fast track to improving data quality since many of its steps mirror activities
  you’ll find in any data quality management process, such as data cleansing, data profiling,
 data integration, and more.
+
   2.It Handles Missing Data
 -There are several reasons why a data collection may be missing values (particular fields of data). 
 -Data practitioners must determine if it’s best to reject records with missing values, ignore them,
  or fill them in with an estimated value.
-  3.It Normalizes and Scales Data
--Dependent and independent variables change on separate scales, or one changes linearly while another
- changes exponentially.
- -Normalizing and scaling help to modify data in a way that allows computers to extract a meaningful 
- link between these variables.
- -Salary, for example, might be a multiple-figure digit, whereas age is expressed in double digits. 
-  4.It Eliminates Duplicate Records
+
