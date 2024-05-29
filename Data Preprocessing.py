a796dea76223d56d844f890673611baf8d578bb4
unknown <njerivictory52@gmail.com>
2024-05-29 16:09:09 +0300

    commit

--- Data Preprocessing.py
+++ Data Preprocessing.py
@@ -1,2 +1,12 @@
 '''Data preprocessing is the process of evaluating, filtering, manipulating, 
 and encoding data so that a machine learning algorithm can understand it and use the resulting output. 
+-The major goal of data preprocessing is to eliminate data issues such as :
+  -missing values, 
+  -improve data quality and 
+  -make the data useful for machine learning purposes.
+
+  Importance of Data Preprocessing 
+
+  As the adage goes, “If garbage goes in, garbage comes out.” 
+  -Your data project will only be as successful as the input 
+  data you feed into your machine learning algorithms.
