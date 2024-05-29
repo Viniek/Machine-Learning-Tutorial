5b8153115749ef92fd814eb7c201e651d59015fc
unknown <njerivictory52@gmail.com>
2024-05-29 15:23:48 +0300

    commit

--- introduction.py
+++ introduction.py
@@ -1,4 +1,6 @@
 """-Machine Learning  often  abbreviated as ML, is a branch of Artificial Intelligence (AI)
 that works on algorithm developments and statistical models that allow computers to learn from 
 data and make predictions or decisions without being explicitly programmed.
+-Machine learning focuses primarily on the creation of algorithms that enable a computer to 
+independently learn from data and previous experiences.
 """
