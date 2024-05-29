daf344cc7ba26538b999809ce234626ec785245b
unknown <njerivictory52@gmail.com>
2024-05-29 16:02:31 +0300

    commit

---
+++ Data Preprocessing.py
@@ -0,0 +1,2 @@
+'''Data preprocessing is the process of evaluating, filtering, manipulating, 
+and encoding data so that a machine learning algorithm can understand it and use the resulting output. 
