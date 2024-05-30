80a594f72af68081ea7276f446687b2a73d1d7f9
unknown <njerivictory52@gmail.com>
2024-05-30 13:27:46 +0300

    commit

--- Data Preprocessing.py
+++ Data Preprocessing.py
@@ -24,6 +24,7 @@ data integration, and more.
  or fill them in with an estimated value.
 
         3.It Normalizes and Scales Data
+        
 -Dependent and independent variables change on separate scales, or one changes linearly while another
  changes exponentially.
  -Normalizing and scaling help to modify data in a way that allows computers to extract a meaningful 
