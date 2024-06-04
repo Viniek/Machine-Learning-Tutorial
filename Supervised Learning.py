cc6ccdf711f3307f139b1918babd7a0ffd19ce81
unknown <njerivictory52@gmail.com>
2024-06-04 15:13:43 +0300

    --

--- Supervised Learning.py
+++ Supervised Learning.py
@@ -8,7 +8,21 @@
 well "labelled" training data, and on basis of that data, machines predict the output. 
 -The labelled data means some input data is already tagged with the correct output.
 
-                          Uses
+
+Example:
+
+Let’s say you have a fruit basket that you want to identify. The machine would first analyze the image to
+ extract features such as its shape, color, and texture. Then, it would compare these features to the 
+ features of the fruits it has already learned about. If the new image’s features are most similar
+  to those of an apple, the machine would predict that the fruit is an apple.
+
+For instance, suppose you are given a basket filled with different kinds of fruits. Now the first step is to train the machine with all the different fruits one by one like this: 
+
+If the shape of the object is rounded and has a depression at the top, is red in color, then it will be labeled as –Apple.
+If the shape of the object is a long curving cylinder having Green-Yellow color, then it will be labeled as –Banana. 
+
+
+                          Uses of s
  1.Risk Assessment
  2.Image classification
  3.Fraud Detection
