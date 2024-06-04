f5c559ed534763f90cd5106b59944419353c5cb0
unknown <njerivictory52@gmail.com>
2024-06-04 14:50:45 +0300

    --

---
+++ Supervised Learning.py
@@ -0,0 +1,3 @@
+'''Supervised Machine Learning
+
+Supervised learning is the types of machine learning in which machines are trained using well "labelled" training data, and on basis of that data, machines predict the output. The labelled data means some input data is already tagged with the correct output.
