9d513ca11e9cfd649d4841725923447385768e12
unknown <njerivictory52@gmail.com>
2024-06-04 15:37:54 +0300

    --

--- T3-Supervised Learning.py/T3B-Regression.py
+++ T3-Supervised Learning.py/T3B-Regression.py
@@ -1,8 +1,8 @@
 '''Linear regression is a type of supervised machine learning algorithm that computes the linear 
 relationship between the dependent variable and one or more independent features by fitting a linear 
 equation to observed data.
-
--When there is only one independent feature, it is known as Simple Linear Regression, and when there are more than one feature, it is known as Multiple Linear Regression.
+-When there is only one independent feature, it is known as Simple Linear Regression, and
+ when there are more than one feature, it is known as Multiple Linear Regression.
 
             common regression algorithms include:
   1.Linear Regression      
