8a8a9b38df111ffecb9a7ba4c783543f7e3870bb
unknown <njerivictory52@gmail.com>
2024-06-04 15:56:25 +0300

    simple linear regression

--- T3-Supervised Learning.py/T3B-Regression.py
+++ T3-Supervised Learning.py/T3B-Regression.py
@@ -17,8 +17,19 @@ Y is the dependent variable
 X is the independent variable
 β0 is the intercept
 β1 is the slope
-ε  is the error term. (For a good model it will be negligible)
+ε  is the error term. (For a good model it will be negligible
 
+Example of Simple Linear Regression:
+-Here we are taking a dataset that has two variables: salary (dependent variable) and experience 
+(Independent variable). 
+
+  The goals of this problem is:
+-We want to find out if there is any correlation between these two variables
+-We will find the best fit line for the dataset.
+-How the dependent variable is changing by changing the independent variable.
+-In this section, we will create a Simple Linear Regression model to find out the best fitting line 
+for representing the relationship between these two variables.
+Step-1: Data Pre-processing
 ​
  
 
