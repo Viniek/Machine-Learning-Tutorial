22940844d9d75494e460ea851e4ddc4191116186
unknown <njerivictory52@gmail.com>
2024-06-04 15:32:08 +0300

    --

--- T3-Supervised Learning.py/T3B-Regression.py
+++ T3-Supervised Learning.py/T3B-Regression.py
@@ -1,3 +1,4 @@
 '''Regression is used to predict continuous values, such as house prices, stock prices, or customer churn. 
 
-common regression algorithms include:
+            common regression algorithms include:
+  1.Linear Regression          
