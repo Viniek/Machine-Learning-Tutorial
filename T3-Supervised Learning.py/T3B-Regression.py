abb62c5006620ec2f003d55ad31548f1aa6f417f
unknown <njerivictory52@gmail.com>
2024-06-04 16:03:35 +0300

    --

--- T3-Supervised Learning.py/T3B-Regression.py
+++ T3-Supervised Learning.py/T3B-Regression.py
@@ -1,37 +1,44 @@
-'''Linear regression is a type of supervised machine learning algorithm that computes the linear 
-relationship between the dependent variable and one or more independent features by fitting a linear 
-equation to observed data.
--When there is only one independent feature, it is known as Simple Linear Regression, and
- when there are more than one feature, it is known as Multiple Linear Regression.
- -Similarly, when there is only one dependent variable, it is considered Univariate Linear Regression, 
- while when there are more than one dependent variables, it is known as Multivariate Regression.
+# Linear regression is a type of supervised machine learning algorithm that computes the linear 
+# relationship between the dependent variable and one or more independent features by fitting a linear 
+# equation to observed data.
+# -When there is only one independent feature, it is known as Simple Linear Regression, and
+#  when there are more than one feature, it is known as Multiple Linear Regression.
+#  -Similarly, when there is only one dependent variable, it is considered Univariate Linear Regression, 
+#  while when there are more than one dependent variables, it is known as Multivariate Regression.
 
-           Types of Linear Regression
-  1. Simple Linear Regression 
+#            Types of Linear Regression
+#   1. Simple Linear Regression 
 
-  -This is the simplest form of linear regression, and it involves only one independent variable and 
-  one dependent variable. The equation for simple linear regression is:   
-  y=β0 +β1 X+ε 
-  where:
-Y is the dependent variable
-X is the independent variable
-β0 is the intercept
-β1 is the slope
-ε  is the error term. (For a good model it will be negligible
+#   -This is the simplest form of linear regression, and it involves only one independent variable and 
+#   one dependent variable. The equation for simple linear regression is:   
+#   y=β0 +β1 X+ε 
+#   where:
+# Y is the dependent variable
+# X is the independent variable
+# β0 is the intercept
+# β1 is the slope
+# ε  is the error term. (For a good model it will be negligible
 
-Example of Simple Linear Regression:
--Here we are taking a dataset that has two variables: salary (dependent variable) and experience 
-(Independent variable). 
+# Example of Simple Linear Regression:
+# -Here we are taking a dataset that has two variables: salary (dependent variable) and experience 
+# (Independent variable). 
 
-  The goals of this problem is:
--We want to find out if there is any correlation between these two variables
--We will find the best fit line for the dataset.
--How the dependent variable is changing by changing the independent variable.
--In this section, we will create a Simple Linear Regression model to find out the best fitting line 
-for representing the relationship between these two variables.
-Step-1: Data Pre-processing
-​
- 
+#   The goals of this problem is:
+# -We want to find out if there is any correlation between these two variables
+# -We will find the best fit line for the dataset.
+# -How the dependent variable is changing by changing the independent variable.
+# -In this section, we will create a Simple Linear Regression model to find out the best fitting line 
+# for representing the relationship between these two variables.
+# Step-1: Data Pre-processing
+# ​
+#  First, we will import the three important libraries, which will help us for loading the dataset,
+#   plotting the graphs, and creating the Simple Linear Regression model.
+   import numpy as nm
+   import matplotlib.pyplot as matp
+   import pandas as pd
+
+#    Next, we will load the dataset into our code:
+  data_set=pd.read_csv("Salary_Data.csv")
 
 ​
  
