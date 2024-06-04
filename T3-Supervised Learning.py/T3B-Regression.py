9876f8aa5cf3cfbe954d9e23dc66da7f77403947
unknown <njerivictory52@gmail.com>
2024-06-04 15:37:19 +0300

    --

--- T3-Supervised Learning.py/T3B-Regression.py
+++ T3-Supervised Learning.py/T3B-Regression.py
@@ -2,5 +2,7 @@
 relationship between the dependent variable and one or more independent features by fitting a linear 
 equation to observed data.
 
+-When there is only one independent feature, it is known as Simple Linear Regression, and when there are more than one feature, it is known as Multiple Linear Regression.
+
             common regression algorithms include:
   1.Linear Regression      
