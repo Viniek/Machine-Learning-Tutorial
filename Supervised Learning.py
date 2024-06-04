8dfe8eed66022933eb7e445696d3b8d2267fab10
unknown <njerivictory52@gmail.com>
2024-06-04 15:02:32 +0300

    --

--- Supervised Learning.py
+++ Supervised Learning.py
@@ -14,4 +14,4 @@ well "labelled" training data, and on basis of that data, machines predict the o
  3.Fraud Detection
  4.spam filtering
                  How Supervised Learning Works
- 1.                
+ 1.Models are trained using labelled dataset            
