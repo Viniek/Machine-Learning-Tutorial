dcf8a38da99c343044611b33d74545a309d125f0
unknown <njerivictory52@gmail.com>
2024-06-04 14:54:09 +0300

    --

--- Supervised Learning.py
+++ Supervised Learning.py
@@ -1,5 +1,9 @@
 '''Supervised Machine Learning
 
--Supervised learning is the types of machine learning in which machines are trained using 
+-Supervised learning is a process of providing input data as well as correct output data to the machine
+ learning model.
+-The aim of a supervised learning algorithm is to find a mapping function to map the input
+  variable(x) with the output variable(y).
+-Supervised learning is the type of machine learning in which machines are trained using 
 well "labelled" training data, and on basis of that data, machines predict the output. 
 -The labelled data means some input data is already tagged with the correct output.
